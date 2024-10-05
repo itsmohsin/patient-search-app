4016342dceea9f0b0f67f9eeda5dd5f7e4b79cc9
Mohsin Khan <mohsinsmannat@gmail.com>
2024-10-05 12:54:28 +0530

    added phone login

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vivaskintech/main.dart';
+import 'package:vivaskintech/oldmain.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
