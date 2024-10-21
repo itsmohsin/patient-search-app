b0c8bdced2a4f1915aa226cbfde404662cfc9170
itsmohsin <mohsinsmannat@gmail.com>
2024-10-21 19:59:46 +0530

    add some data

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:vivaskintech/draftcreatemain.dart';
 
-import 'package:vivaskintech/oldmain.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( MyApp());
+    await tester.pumpWidget( PatientSearchApp ());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
